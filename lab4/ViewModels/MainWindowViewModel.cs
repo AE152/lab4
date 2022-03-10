a92e6bf4f2e86046091093fe38d58b799d4c2f98
AE152 <ggoollll35@gmail.com>
2022-03-10 23:59:41 +0700

    MainWindowViewModel.cs не доделан

--- lab4/ViewModels/MainWindowViewModel.cs
+++ lab4/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,55 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ReactiveUI;
+using lab4.Models;
+
 
 namespace lab4.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+       public string greeting = "";
+
+        
+        public MainWindowViewModel()
+        {
+            OnClickCommand = ReactiveCommand.Create<string, string>((str) => Greeting = str);
+        }
+
+        public string Greeting
+        {
+            set
+            {
+                if (value != "=")
+                    this.RaiseAndSetIfChanged(ref greeting, greeting + value);
+                else
+                {
+                    this.RaiseAndSetIfChanged(ref greeting, greeting + value);
+                    int i = 1;
+                    string x1 = "";
+                    string x2 = "";
+                    while (greeting[i] != '=')
+                    {
+                        while(greeting[i] != '+' || greeting[i] != '-' || greeting[i] != '*' || greeting[i] != '/')
+                        {
+                            x1 += greeting[i];
+                            i++;
+                        }
+                        x2 += greeting[i];
+                        i++;
+                    }
+                    RomanNumber x11 = new RomanNumberExtend(x1);
+                    RomanNumber x22 = new RomanNumberExtend(x2);
+                    string result = (x11 + x22).ToString();
+                }
+
+            }
+            get
+            {
+                return greeting;
+            }
+        }
+        public ReactiveCommand<string, string> OnClickCommand { get; }
     }
 }
