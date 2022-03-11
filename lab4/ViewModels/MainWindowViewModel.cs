bd23a12dc6d05a345a1e488828b1fb0470a7fcc3
AE152 <ggoollll35@gmail.com>
2022-03-12 00:49:07 +0700

    работает с двумя цифрами

--- lab4/ViewModels/MainWindowViewModel.cs
+++ lab4/ViewModels/MainWindowViewModel.cs
@@ -25,23 +25,8 @@ namespace lab4.ViewModels
                     this.RaiseAndSetIfChanged(ref greeting, greeting + value);
                 else
                 {
-                    this.RaiseAndSetIfChanged(ref greeting, greeting + value);
-                    int i = 1;
-                    string x1 = "";
-                    string x2 = "";
-                    while (greeting[i] != '=')
-                    {
-                        while(greeting[i] != '+' || greeting[i] != '-' || greeting[i] != '*' || greeting[i] != '/')
-                        {
-                            x1 += greeting[i];
-                            i++;
-                        }
-                        x2 += greeting[i];
-                        i++;
-                    }
-                    RomanNumber x11 = new RomanNumberExtend(x1);
-                    RomanNumber x22 = new RomanNumberExtend(x2);
-                    string result = (x11 + x22).ToString();
+                    this.RaiseAndSetIfChanged(ref greeting, greeting + "=" + Calc(greeting));
+                    greeting = "";//отчишаем строку для следующего выражения                    
                 }
 
             }
@@ -49,6 +34,105 @@ namespace lab4.ViewModels
             {
                 return greeting;
             }
+        }
+
+        private string Obr_polsk_zapis(string virazhenie)
+        {
+            string polsk_zap = "";
+            Stack<string> operations = new Stack<string>();
+            for (int i = 0; i < virazhenie.Length; i++)
+            {
+                switch (virazhenie[i])
+                {
+                    case '+':
+                        operations.Push("+");
+                        polsk_zap += " ";
+                        break;
+                    case '-':
+                        operations.Push("-");
+                        polsk_zap += " ";
+                        break;
+                    case '*':
+                        operations.Push("*");
+                        polsk_zap += " ";
+                        break;
+                    case '/':
+                        operations.Push("/");
+                        polsk_zap += " ";
+                        break;
+                    default:
+                        polsk_zap += virazhenie[i];
+                        break;
+                }
+            }
+            foreach (string operation in operations)
+            {
+                polsk_zap += " ";
+                polsk_zap += operation;
+            }
+            return polsk_zap;
+        }
+
+        //Стековый калькулятор, работает через обратную польскую запись, приоритеты не реализованы, корректно работает с двумя цифрами
+        private string Calc(string virazhenie)
+        {
+            string polsk_zap = Obr_polsk_zapis(virazhenie);
+            Stack<string> otvet = new Stack<string>();
+            for(int j = 0; j < polsk_zap.Length; j++)
+            {
+                if (polsk_zap[j] == '+' || polsk_zap[j] == '-' || polsk_zap[j] == '*' || polsk_zap[j] == '/')
+                {
+                    string num1 = "";
+                    string num2 = "";
+                    otvet.Pop();//проходим пробел
+                    while(otvet.Peek() != " ")
+                    {
+                        num1 += otvet.Pop();
+                    }
+                    otvet.Pop();//проходим пробел
+                    while ( (otvet.Count != 0) && (otvet.Peek() != " ") )
+                    {
+                        
+                        num2 += otvet.Pop();
+                    }
+                    string num1_reverse = "";
+                    for(int k = num1.Length-1; k >= 0; k--)
+                    {
+                        num1_reverse += num1[k]; 
+                    }
+                    string num2_reverse = "";
+                    for (int k = num2.Length - 1; k >= 0; k--)
+                    {
+                        num2_reverse += num2[k];
+                    }
+                    RomanNumber num2_Rom = new RomanNumberExtend(num1_reverse);
+                    RomanNumber num1_Rom = new RomanNumberExtend(num2_reverse);
+                    switch (polsk_zap[j])
+                    {
+                        case '+':
+                            otvet.Push((num1_Rom + num2_Rom).ToString());
+                            break;
+                        case '-':
+                            otvet.Push((num1_Rom - num2_Rom).ToString());
+                            break;
+                        case '*':
+                            otvet.Push((num1_Rom * num2_Rom).ToString());
+                            break;
+                        case '/':
+                            otvet.Push((num1_Rom / num2_Rom).ToString());
+                            break;
+
+                    }
+                }
+                else
+                    if(polsk_zap[j] == ' ')
+                    {
+                        otvet.Push(" ");
+                    }
+                    else
+                        otvet.Push(polsk_zap.Substring(j,1));
+            }
+            return otvet.Pop(); 
         }
         public ReactiveCommand<string, string> OnClickCommand { get; }
     }
